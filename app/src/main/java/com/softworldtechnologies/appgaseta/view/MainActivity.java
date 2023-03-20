4891e3ae174f4b76784f359d8ef0c390cc14f854
Islan Medeiros <islan.developer.pb@gmail.com>
2023-03-20 11:03:37 -0300

    Adicionado Classe Util com informações importantes

--- app/src/main/java/com/softworldtechnologies/appgaseta/view/MainActivity.java
+++ app/src/main/java/com/softworldtechnologies/appgaseta/view/MainActivity.java
@@ -3,8 +3,10 @@ package com.softworldtechnologies.appgaseta.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.softworldtechnologies.appgaseta.R;
+import com.softworldtechnologies.appgaseta.apoio.UtilGasEta;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+
+        Toast.makeText ( this, UtilGasEta.calcularMelhorOpcao ( 5.12, 3.99 ), Toast.LENGTH_SHORT ).show ( );
     }
 }
